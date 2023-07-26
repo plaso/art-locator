766196be018b8a1d02a2c4bb9b0b27dc0daece5f
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-07-26 20:55:32 +0200

    feat: auth middleware

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,30 +4,32 @@ const authController = require('../controllers/auth.controller');
 const usersController = require('../controllers/users.controller');
 const artworksController = require('../controllers/artworks.controller');
 
+const authMiddleware = require('../middlewares/auth.middleware');
+
 router.get('/', miscController.getHome);
 
 /* Auth */
 
-router.get('/register', authController.register);
-router.post('/register', authController.doRegister);
+router.get('/register', authMiddleware.isUnauthenticated, authController.register);
+router.post('/register', authMiddleware.isUnauthenticated, authController.doRegister);
 
-router.get('/login', authController.login);
-router.post('/login', authController.doLogin);
+router.get('/login', authMiddleware.isUnauthenticated, authController.login);
+router.post('/login', authMiddleware.isUnauthenticated, authController.doLogin);
 
-router.get('/logout', authController.logout);
+router.get('/logout', authMiddleware.isAuthenticated, authController.logout);
 
-router.get('/login/google', authController.loginGoogle);
-router.get('/authenticate/google/cb', authController.doLoginGoogle);
+router.get('/login/google', authMiddleware.isUnauthenticated, authController.loginGoogle);
+router.get('/authenticate/google/cb', authMiddleware.isUnauthenticated, authController.doLoginGoogle);
 
 /* User */
 
-router.get('/profile', usersController.profile);
+router.get('/profile', authMiddleware.isAuthenticated, usersController.profile);
 
 /* Artwork */
 
 router.get('/artworks', artworksController.list);
-router.post('/artworks/create', artworksController.doCreate);
-router.get('/artworks/create', artworksController.create);
+router.post('/artworks/create', authMiddleware.isAuthenticated, artworksController.doCreate);
+router.get('/artworks/create', authMiddleware.isAuthenticated, artworksController.create);
 router.get('/artworks/:id', artworksController.detail);
 
 module.exports = router;
